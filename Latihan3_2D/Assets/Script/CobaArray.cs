57bf6400ec97ee7d81512e6591e63c2f19a8874a
hajirriski <hajir.riski@gmail.com>
2021-09-20 00:04:49 +0700

    up latihan3

---
+++ Latihan3_2D/Assets/Script/CobaArray.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CobaArray : MonoBehaviour
+{
+    public string [] items;
+    void Start()
+    {
+        for (int i = 0; i < items.Length; i++){
+            Debug.Log (items[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
